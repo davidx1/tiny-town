60a112f4f4cad339914bf8b64d2fef0379f1f451
David Xu <davidxu1991@gmail.com>
2024-03-07 10:01:36 +1300

    feat: removing the next export command

---
+++ src/app/end/page.tsx
@@ -0,0 +1,11 @@
+export default function NewPage() {
+  return (
+    <div>
+      <h1>{"Congratulations on Beating the Game!"}</h1>
+      <p>{"This has been a fun side project by David Xu"}</p>
+      <p>
+        {"Check out the source code here: https://github.com/davidx1/tiny-town"}
+      </p>
+    </div>
+  );
+}
