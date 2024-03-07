cb018ff71ecc59b327967a0e219bc18ece7aea20
David Xu <davidxu1991@gmail.com>
2024-03-07 15:40:45 +1300

    feat: added device detection and warning label

--- src/app/opening/page.tsx
+++ src/app/opening/page.tsx
@@ -13,6 +13,7 @@ import {
 } from "../../hooks/useInventoryData";
 import { usePlotData, PlotContext } from "../../hooks/usePlotData";
 import { useContext, useEffect, useState } from "react";
+import { isMobile } from "react-device-detect";
 
 function PageInner() {
   const textareaValue = useTextarea(openingConvoRecord);
@@ -38,9 +39,18 @@ function PageInner() {
         initialPositionRecords={openingInitialPositionRecord}
         triggerRecord={openingTriggerRecord}
       />
-      {showControl && (
+      {showControl && !isMobile && (
         <div className="bg-gray-800/80 absolute w-1/2 aspect-3/2 z-40 top-1/4 left-1/4 p-8">
           <div className="h-full bg-control-instruction bg-cover"></div>
+        </div>
+      )}
+      {isMobile && (
+        <div className="bg-gray-800/80 absolute w-2/3 z-40 top-1/3 left-1/6 px-8 py-8 flex flex-col items-center text-center">
+          <h1 className="text-white text-xl mb-4">Device Unsupported</h1>
+          <p className="text-gray-200">
+            Mobile devices are not supported yet. Please try again on a
+            computer.
+          </p>
         </div>
       )}
     </TextareaContext.Provider>
